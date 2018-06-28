0b37dd1ce1f17e237b2e88e71a8dbad9d51208fa
leeburgess67 <leeburgessmail@gmail.com>
2018-06-28 15:34:29 +0100

    tests passing

--- src/test/java/DataAdminTest.java
+++ src/test/java/DataAdminTest.java
@@ -21,4 +21,17 @@ public class DataAdminTest {
     public void hasNationalInsurance(){
         assertEquals("JH1254533", dataAdmin.getNationalInsurance());
     }
+
+
+    @Test
+    public void canRaiseSalary(){
+        dataAdmin.raiseSalary(1.00);
+        assertEquals(20200.00,dataAdmin.getSalary(), 1);
+    }
+
+    @Test
+    public void canPayBonus() {
+        dataAdmin.payBonus();
+        assertEquals(20200.00, dataAdmin.getSalary(), 1);
+    }
 }
--- src/test/java/DeveloperTest.java
+++ src/test/java/DeveloperTest.java
@@ -21,5 +21,17 @@ public class DeveloperTest {
     public void hasNationalInsurance(){
         assertEquals("JH1234313", developer.getNationalInsurance());
     }
+
+    @Test
+    public void canRaiseSalary(){
+        developer.raiseSalary(1.00);
+        assertEquals(23230.00,developer.getSalary(), 1);
+    }
+
+    @Test
+    public void canPayBonus() {
+        developer.payBonus();
+        assertEquals(23230.00, developer.getSalary(), 1);
+    }
 }
 
